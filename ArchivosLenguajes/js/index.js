dbc3dc46ff9a6e682592c58c1430f63347423cd9
Jennyfer Caucota <caucotafran@gmail.com>
2023-11-07 07:54:11 -0300

    7/11/23
    footer

--- ArchivosLenguajes/js/index.js
+++ ArchivosLenguajes/js/index.js
@@ -1,54 +1,77 @@
-/* banner de img */
-/*const carousel = document.querySelector('.carousel2');
-  const dots = document.querySelectorAll('.dot');
+const carousel = document.querySelector('.carousel');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const imageCount = carousel.children.length;
+let currentIndex = 0;
 
-  let currentIndex = 0;
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1.4) % imageCount;
+  updateCarousel();
+});
 
-  function updateCarousel() {
-    const offset = -currentIndex * 103; // Cambiado a 100
-    carousel.style.transform = `translateX(${offset}%)`; // Cambiado a %
-    dots.forEach((dot, index) => {
-      dot.classList.toggle('active', index === currentIndex);
-    });
-  }*/
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 2 + imageCount) % imageCount;
+  updateCarousel();
+});
 
-  /*function nextSlide() {
-    currentIndex = (currentIndex + 7) % 3; 
+function updateCarousel() {
+  const translateX = -currentIndex * 12; // 12% para cada imagen
+  carousel.style.transition = "transform 0.5s ease-in-out";
+  carousel.style.transform = `translateX(${translateX}%)`;
+
+  // Restablecer la transición después de que termine la animación
+  setTimeout(() => {
+    carousel.style.transition = "none";
+  }, 500);
+ } // 0.4 segundos (ajusta según la duración de tu animación)
+
+ // para las pantallas mas pequeñas
+ if (window.matchMedia('(max-width: 768px)').matches) {
+  // Modifica el comportamiento en pantallas más pequeñas
+  nextButton.addEventListener('click', () => {
+    // Lógica diferente para pantallas más pequeñas
+    currentIndex = (currentIndex + 1) % imageCount; // Mover dos imágenes a la vez
+
+    if (currentIndex === 0) {
+      currentIndex = imageCount - 1; // Ir al penúltimo conjunto de dos imágenes al llegar al principio
+    }
     updateCarousel();
-  }
-
-  dots.forEach((dot, index) => {
-    dot.addEventListener('click', () => {
-      currentIndex = index;
-      updateCarousel();
-    });
   });
 
-  setInterval(nextSlide, 5000); // Cambiar automáticamente cada 5 segundos (en milisegundos)
-  */
-  /* acessos rapidos */
+  nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % imageCount; // Mover una imagen a la vez
+  updateCarousel();
+});
 
-  /**$(document).ready(function() {
-    // Cuando se pasa el cursor sobre un elemento del menú
-    $('.item-menu-aguas a').hover(
-    function() {
-      // Mostrar el cuadro de información
-      $(this).find('.menu-title, .img-mega-menu').show();
-      
-      // Crear y posicionar el cuadro de información
-      var infoBox = $('<div class="menu-info-box"></div>');
-      infoBox.append($(this).find('.menu-title').text());
-      infoBox.append($(this).find('.img-mega-menu').clone());
-      infoBox.css({
-        top: $(this).offset().top + $(this).outerHeight() + 10,
-        left: $(this).offset().left
-      });
-      $('body').append(infoBox);
-    },
-    function() {
-      // Ocultar el cuadro de información al quitar el cursor
-      $(this).find('.menu-title, .img-mega-menu').hide();
-      $('.menu-info-box').remove();
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + imageCount) % imageCount; // Mover una imagen a la vez
+  updateCarousel();
+
+  
+    if (currentIndex === imageCount - 1) {
+      currentIndex = 2; // Ir al segundo conjunto de dos imágenes al llegar al final
     }
-    );
-    });*/
+    updateCarousel();
+  });
+}
+
+
+/* carrousel de imagenes */
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".carousel-slide");
+  let currentSlide = 0;
+
+  function showSlide(slideIndex) {
+      slides[currentSlide].style.display = "none";
+      currentSlide = (slideIndex + slides.length) % slides.length;
+      slides[currentSlide].style.display = "block";
+  }
+
+  function nextSlide() {
+      showSlide(currentSlide + 1);
+  }
+
+  // Inicia el carrusel y configura el intervalo de cambio
+  showSlide(currentSlide);
+  setInterval(nextSlide, 4000); // Cambiar cada 4 segundos (4000 ms)
+});
